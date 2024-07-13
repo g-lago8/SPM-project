920c0d00e54821ca8583238ffa8f1c11a87170c3
Giacomo Lagomarsini <lagomarsinigiacomo@gmail.com>
2024-07-13 18:43:41 +0200

    fixed script

--- scripts/strong_scaling.sh
+++ scripts/strong_scaling.sh
@@ -2,15 +2,16 @@
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <problem_size> <blocksize> <on_demand> <n_tries> <thread_list>"
+    echo "Usage: $0 <program> <problem_size> <blocksize> <on_demand> <n_tries> <thread_list>"
     exit 1
 fi
 
-PROBLEM_SIZE=$1
-BLOCKSIZE=$2
-ON_DEMAND=$3
-N_TRIES=$4
-THREAD_LIST=$5
+PROGRAM =$1
+PROBLEM_SIZE=$2
+BLOCKSIZE=$3
+ON_DEMAND=$4
+N_TRIES=$5
+THREAD_LIST=$6
 
 # Check if the provided number of tries is a positive integer
 if ! [[ "$N_TRIES" =~ ^[0-9]+$ ]]; then
@@ -27,6 +28,6 @@ for THREADS in "${THREAD_ARRAY[@]}"; do
     
     for ((i = 1; i <= N_TRIES; i++)); do
         echo "Iteration $i with $THREADS threads"
-        ../out/farm $PROBLEM_SIZE $THREADS $BLOCKSIZE $ON_DEMAND 
+        ../out/$PPROGRAM $PROBLEM_SIZE $THREADS $BLOCKSIZE $ON_DEMAND 
     done
 done
