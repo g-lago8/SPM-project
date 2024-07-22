ffea3a6848bcbf090a0fe8ba46a72d2c5de21a2a
Giacomo Lagomarsini <lagomarsinigiacomo@gmail.com>
2024-07-22 16:07:05 +0200

    switched names

--- src/parallel_mpi2.cpp
+++ src/parallel_mpi2.cpp
@@ -45,6 +45,51 @@ void compute_internal_part(start_end se, size_t diag, vector<vector<double>> &M,
 }
 
 
+void check_first(
+    start_end se,
+    size_t diag,
+    vector<vector<double>> &M,
+    int rank,
+    MPI_Request requests[2],
+    bool * need_row,
+    double col_to_send[],
+    double row_to_receive[]
+){
+     if (M[se.start][se.start+diag - 1] < 0){
+        *need_row = true;
+        MPI_Irecv(&row_to_receive, diag, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &requests[1]);
+    }
+    else{
+        for (auto i{0}; i <diag; i++ ){
+            col_to_send[i] = M[se.start + i ][se.start+diag - 1];
+            cout << rank << ": "<< col_to_send[i] << " ";
+        }
+        cout << endl;
+        MPI_Isend(&col_to_send, diag, MPI_DOUBLE, rank -1, 0, MPI_COMM_WORLD, &requests[1]);
+    }
+}
+
+void check_last( 
+    start_end se,
+    size_t diag, 
+    vector<vector<double>> &M, 
+    int rank, 
+    MPI_Request requests[2], 
+    bool *need_col,
+    double row_to_send[],
+    double col_to_receive[]
+){
+    if (M[se.end +1][se.end + diag] < 0){
+        *need_col =true;
+        MPI_Irecv(&col_to_receive, diag, MPI_DOUBLE, rank +1, 0, MPI_COMM_WORLD, &requests[0]) ;
+    }
+    else{
+        for (auto i{0}; i<diag;i++){
+            row_to_send[i] = M[se.end + 1][se.end + i + 1];
+        }
+        MPI_Isend(&row_to_send, diag,  MPI_DOUBLE, rank +1, 0, MPI_COMM_WORLD, &requests[0]);
+    }
+}
 
 int main(int argc, char *argv[]){
     MPI_Init(&argc, &argv);
@@ -64,104 +109,90 @@ int main(int argc, char *argv[]){
     double row_to_receive[N];
     double col_to_send[N];
     double col_to_receive[N];
+    // initialize the buffers
+    for (size_t i = 0; i < N; i++){
+        row_to_send[i] = 100;
+        row_to_receive[i] = 100;
+        col_to_send[i] = 100;
+        col_to_receive[i] = 100;
+    }
     MPI_Request requests[2];
+    for (size_t i = 0; i < 2; i++){
+        requests[i] = MPI_REQUEST_NULL;
+    }   
     MPI_Request req;
     bool need_row, need_col = false; // these get set to true when in the two if, the operation is a receive. 
     for (size_t diag = 1; diag < N -1; diag ++){
         se = compute_start_end(rank, size, N - diag);
-        
-        // for all but the last process, check if we have the data we need for the last element of the chunk
-        auto n_active_processes = min(size,int( N - diag ));
-        if (rank< n_active_processes -1){
-            if (M[se.end +1][se.end + diag] < 0){
-                need_col =true;
-                MPI_Irecv(&col_to_receive, diag, MPI_DOUBLE, rank +1, 0, MPI_COMM_WORLD, &requests[0]) ;
-            }
-            else{
-                for (auto i{0}; i<diag;i++){
-                    row_to_send[i] = M[se.end + 1][se.end + i + 1];
-                }
+        auto n_active_processes = min(size,int( N - diag )); // number of active processes (typically = size, but can be less for the last few iterations)
 
-                MPI_Isend(&row_to_send, diag,  MPI_DOUBLE, rank +1, 0, MPI_COMM_WORLD, &requests[1]);
-            }
-        }
-        
-        // for all but the first process, check if we have the data we need for the first element of the chunk
-        if (rank !=0){
-            if (M[se.start][se.start+diag - 1] < 0){
-                need_row = true;
-                MPI_Irecv(&row_to_receive, diag, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &requests[1]);
-            }
-            else{
-                for (auto i{0}; i <diag; i++ ){
-                    col_to_send[i] = M[se.start + i ][se.start+diag - 1];
-                }
-                MPI_Isend(&col_to_send, diag, MPI_DOUBLE, rank -1, 0, MPI_COMM_WORLD, &requests[0]);
+        // first process does not need to checck first
+        if (rank == 0){
+            if (n_active_processes > 1){
+               check_last(se, diag, M, rank, requests, &need_col, row_to_send, col_to_receive); 
             }
         }
 
+        // last process does not need to check last
+        if (rank == n_active_processes -1){
+            if (n_active_processes > 1){
+                check_first(se, diag, M, rank, requests, &need_row, col_to_send, row_to_receive);
+            }
+        }
+
+        // all other processes need to check both
+        if (rank > 0 && rank < n_active_processes -1){
+            check_first(se, diag, M, rank, requests, &need_row, col_to_send, row_to_receive);
+            check_last(se, diag, M, rank, requests, &need_col, row_to_send, col_to_receive);
+        }
         compute_internal_part(se, diag, M, N);
-        int index ;
-        MPI_Waitany(2, requests, &index, MPI_STATUS_IGNORE);    
-        double temp = 0;
-        size_t start_row =se.start;
-        auto start_col = start_row + diag;
-        if (need_row){ // we received the row, not sent it
-            for (size_t j =0; j <diag; j ++){
-                M[start_row][start_row + j] = row_to_receive[j];
-            }
-        }
-        for (size_t j = 0; j < diag; j++) {
-            temp += M[start_row][start_row + j] * M[start_col - j][start_col];
-        }
-        M[start_row][start_col] = temp;
+        // compute the first element 
+ 
 
-        // the same for the column
+        if ( rank < n_active_processes){
 
-        auto end_row = se.end;
-        auto end_col = end_row + diag;
-        temp=0;
-        if (need_col){
-            // sleep for 1 second
-            for (auto j = 0; j<diag; j ++ ){
-                M[end_row +j +1][end_col] = col_to_receive[j];
+            double temp = 0;
+            size_t start_row =se.start;
+            auto start_col = start_row + diag;
+            if (need_row){ 
+                MPI_Wait(&requests[1], MPI_STATUS_IGNORE);
+                for (size_t j =0; j <diag; j ++){
+                    M[start_row][start_row + j] = row_to_receive[j];
+                }
             }
+            for (size_t j = 0; j < diag; j++) {
+                temp += M[start_row][start_row + j] * M[start_col - j][start_col];
+            }
+            M[start_row][start_col] = temp;
+            cout <<"diag " << diag << " rank " << rank << " computed the first element" << "(" << start_row << "," << start_col << ")" << endl;
+            // compute the last element
+            auto end_row = se.end;
+            auto end_col = end_row + diag;
+            temp=0;
+            if (need_col){
+                cout << "reuqest "<< &requests[0] << endl;   
+                MPI_Wait(&requests[0], MPI_STATUS_IGNORE);
+                for (auto j = 0; j<diag; j ++ ){
+                    cout << col_to_receive[j] << " ";
+                    M[end_row +j +1][end_col] = col_to_receive[j];
+                }
+                cout << endl;
+            }
+            for (size_t j = 0; j < diag; j++) {
+                temp += M[end_row][end_row + j] * M[end_col - j][end_col];
+            }
+            M[end_row][end_col] = temp;
+            cout << "diag " << diag << " rank " << rank << " computed the last element" << "(" << end_row << "," << end_col << ")" << endl;
         }
-        for (size_t j = 0; j < diag; j++) {
-            temp += M[end_row][end_row + j] * M[end_col - j][end_col];
-        }
-        M[end_row][end_col] = temp;
-        if (rank ==2) {
+        if (rank == 0){
             print_matrix(M);
-            cout << "-------------------"<<endl;
+            cout << endl;
         }
-        need_col = false;
+        // reset the flags
         need_row = false;
-        auto foo = 0;
+        need_col = false;
         MPI_Barrier(MPI_COMM_WORLD);
     }
 
-    if (rank ==size -1){
-        for (auto i = 0; i < N -1; i++){
-            col_to_send[i] = M[i +1][N-1];
-        }
-        MPI_Send(&col_to_send, N - 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-    }
-
-    if (rank ==0){
-        MPI_Recv(&col_to_receive, N - 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        auto temp = 0;
-        for (auto i = 0; i < N -1; i++){
-            M[i +1][N-1] = col_to_receive[i];
-        }
-        for (auto i = 0; i < N -1; i++){
-            temp += M[0][i] * M[N-1 - i][N-1];
-        }
-        M[0][N-1] = temp;
-        print_matrix(M);
-    
-    }
     MPI_Finalize();
-
-    return 0;
 }
