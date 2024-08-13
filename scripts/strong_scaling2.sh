0af845192cf38a233a68d0fb052b7cba80602bd8
g-lago8 <lagomarsinigiacomo@gmail.com>
2024-08-13 18:31:57 +0200

    modify scripts

--- scripts/strong_scaling2.sh
+++ scripts/strong_scaling2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <problem_size> <n_tries> <thread_list>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <program> <problem_size> <n_tries> <thread_list>"
     exit 1
 fi
 
-
-PROBLEM_SIZE=$1
-N_TRIES=$2
-THREAD_LIST=$3
+PROGRAM=$1
+PROBLEM_SIZE=$2
+N_TRIES=$3
+THREAD_LIST=$4
 
 # Check if the provided number of tries is a positive integer
 if ! [[ "$N_TRIES" =~ ^[0-9]+$ ]]; then
@@ -22,10 +22,10 @@ IFS=',' read -r -a THREAD_ARRAY <<< "$THREAD_LIST"
 
 # Run the program for each number of threads in the list
 for THREADS in "${THREAD_ARRAY[@]}"; do
-    echo " N=$PROBLEM_SIZE, blocksize=$BLOCKSIZE, on_demand=$ON_DEMAND, threads=$THREADS for $N_TRIES times"
+    echo " N=$PROBLEM_SIZE, threads=$THREADS for $N_TRIES times"
     
     for ((i = 1; i <= N_TRIES; i++)); do
         echo "Iteration $i with $THREADS threads"
-        ../out/parallel_ff2 $PROBLEM_SIZE $THREADS
+        ../out/$PROGRAM $PROBLEM_SIZE $THREADS
     done
 done
