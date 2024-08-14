47fe167166f2e8269d45fb0820a166687eae21bd
Giacomo <lagomarsinigiacomo@gmail.com>
2024-08-14 12:05:03 +0200

    fix print

--- src/parallel_mpi.cpp
+++ src/parallel_mpi.cpp
@@ -104,7 +104,8 @@ int main(int argc, char *argv[]){
     }
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    std::cout << "using " << size << " processes" <<std::endl;
+    if (rank == 0)
+        std::cout << "using " << size << " processes" <<std::endl;
     if (size < 2) {
         // abort if there is only one process
         cout << "This program is meant to be run with at least 2 processes" << endl;
