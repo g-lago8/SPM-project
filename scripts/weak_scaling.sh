53f947b3e9e8a8c49faf330521f3b665ba3c135e
Giacomo Lagomarsini <lagomarsinigiacomo@gmail.com>
2024-08-16 11:56:05 +0200

    modify weak scaling script

--- scripts/weak_scaling.sh
+++ scripts/weak_scaling.sh
@@ -2,13 +2,12 @@
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <program> <initial_problem_size> <n_tries> <thread_list>"
+    echo "Usage: $0  <initial_problem_size> <n_tries> <thread_list>"
     exit 1
 fi
-PROGRAM=$1
-PROBLEM_SIZE=$2
-N_TRIES=$3
-THREAD_LIST=$4
+PROBLEM_SIZE=$1
+N_TRIES=$2
+THREAD_LIST=$3
 
 # Check if the provided number of tries is a positive integer
 if ! [[ "$N_TRIES" =~ ^[0-9]+$ ]]; then
@@ -19,23 +18,16 @@ fi
 # Convert thread list to an array
 IFS=',' read -r -a THREAD_ARRAY <<< "$THREAD_LIST"
 
-# Function to calculate the cubic root
-cubic_root() {
-    echo "scale=5; e( l($1) / 3 )" | bc -l
-}
 
 # Run the program for each number of threads in the list
 for THREADS in "${THREAD_ARRAY[@]}"; do
     # Calculate the new problem size
-    CUBIC_ROOT=$(cubic_root $THREADS)
-    NEW_PROBLEM_SIZE=$(echo "$PROBLEM_SIZE * $CUBIC_ROOT" | bc)
-    NEW_PROBLEM_SIZE=${NEW_PROBLEM_SIZE%.*}  # Convert to an integer by truncating the decimal part
 
-    echo "N=$NEW_PROBLEM_SIZE, threads=$THREADS for $N_TRIES times"
+    echo "initial N=$PROBLEM_SIZE, threads=$THREADS for $N_TRIES times"
     
     for ((i = 1; i <= N_TRIES; i++)); do
         echo "Iteration $i with $THREADS threads"
-        ../out/sequential $NEW_PROBLEM_SIZE "../results/weak_scaling_seq.txt"
-        ../out/$PROGRAM $NEW_PROBLEM_SIZE $THREADS "../results/weak_scaling.txt"
+        ../out/sequential $PROBLEM_SIZE "../results/weak_scaling_seq.txt"    # Run the sequential version
+        ../out/weak_scaling $ROBLEM_SIZE $THREADS "../results/weak_scaling.txt" # Run the parallel version with the specified number of threads
     done
 done
